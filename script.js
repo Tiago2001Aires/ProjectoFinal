030bfccdeca3af548359553ccea1bad7c4fe7265
Tiago2001Aires <tiago9aires@gmail.com>
2022-06-09 00:02:24 +0100

    ; em falta

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH=canvas.width=600;
-const CANVAS_HEIGHT=canvas.height=600;
+const CANVAS_WIDTH = canvas.width=600;
+const CANVAS_HEIGHT = canvas.height=600;
 
-const playerImage=new Image();
-playerImage.src='euphus_young_fly.png';
+const playerImage = new Image();
+playerImage.src = 'euphus_young_fly.png';
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(playerImage, 0, 0)
+    ctx.drawImage(playerImage, 0, 0);
     requestAnimationFrame(animate);
 };
 animate();
