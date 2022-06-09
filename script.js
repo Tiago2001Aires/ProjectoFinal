534dbc1161586642d969b62d04dfdf863e9c2e75
Rui Borreicho <180100436@esg.ipsantarem.pt>
2022-06-09 07:09:34 +0100

    criação do efeito de "scroll" do fundo e pequenas alterações nas plataformas

--- script.js
+++ script.js
@@ -48,10 +48,10 @@ class Player {
 }
 
 class Platform {
-    constructor(){
+    constructor({x, y}){
         this.position = {
-            x: 200,
-            y: 100
+            x: x,
+            y: y
         }
         this.width = 200
         this.height = 20
@@ -64,7 +64,8 @@ class Platform {
 }
 
 const player = new Player()
-const platform = new Platform()
+const platforms = [new Platform({x: 200, y:400}), new Platform({x:500, y:500})]
+
 const keys = {
     right: {
         pressed: false
@@ -90,21 +91,39 @@ function animate(){
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     player.update()
-    platform.draw()
+    platforms.forEach((platform) => {
+        platform.draw()
+    })
 
-    if (keys.right.pressed){
+    if (keys.right.pressed && player.position.x < 400){
         player.velocity.x = 5
-    } else if (keys.left.pressed){
+    } else if (keys.left.pressed && player.position.x > 100){
         player.velocity.x = -5
-    } else player.velocity.x = 0
+    } else {
+        player.velocity.x = 0
+
+        if (keys.right.pressed){
+            platforms.forEach((platform) => {
+                platform.position.x -= 5
+            })
+            
+        } else if (keys.left.pressed){
+            platforms.forEach((platform) => {
+                platform.position.x += 5
+            })
+            
+        }
+    }
 
     //player platform collision
-    if (player.position.y + player.height <= platform.position.y 
-        && player.position.y + player.height + player.velocity.y >= platform.position.y
-        && player.position.x + player.width >= platform.position.x
-        && player.position.x  <= platform.position.x + platform.width){
-        player.velocity.y = 0
-    }
+    platforms.forEach((platform) => {
+        if (player.position.y + player.height <= platform.position.y 
+            && player.position.y + player.height + player.velocity.y >= platform.position.y
+            && player.position.x + player.width >= platform.position.x
+            && player.position.x  <= platform.position.x + platform.width){
+                player.velocity.y = 0
+        }
+    })
 };
 
 animate();             
@@ -122,7 +141,7 @@ window.addEventListener('keydown', ({key}) => {
             break
         case 'ArrowUp':
             console.log('up')
-            player.velocity.y -= 20
+            player.velocity.y -= 10
             break
         case 'ArrowDown':
             console.log('down')
@@ -143,7 +162,7 @@ window.addEventListener('keyup', ({key}) => {
             break
         case 'ArrowUp':
             console.log('up')
-            player.velocity.y -= 20
+            player.velocity.y -= 10
             break
         case 'ArrowDown':
             console.log('down')
