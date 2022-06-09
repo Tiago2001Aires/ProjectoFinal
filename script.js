4c2c87f1bfc866cd636ee6db72f893e9dd35de3c
Rui Borreicho <180100436@esg.ipsantarem.pt>
2022-06-09 01:23:53 +0100

    criação do player

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-const canvas = document.getElementById('canvas1');
+const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width=600;
-const CANVAS_HEIGHT = canvas.height=600;
+const CANVAS_WIDTH = window.innerWidth;
+const CANVAS_HEIGHT = window.innerHeight;
+
+canvas.width = CANVAS_WIDTH
+canvas.height = CANVAS_HEIGHT
 
 const playerImage = new Image();
 playerImage.src = 'PngItem_3575839.png';
 
-function animate(){
+class Player {
+    constructor(){
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 30
+        this.height = 30
+    }
+
+    draw(){
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player()
+player.draw()
+
+/*function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(playerImage, 0, 0);
     requestAnimationFrame(animate);
 };
 animate();
-                      
+*/                     
