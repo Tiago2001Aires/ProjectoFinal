f3163c74caca64ddc0eefb5436610de0cc420048
Rui Borreicho <180100436@esg.ipsantarem.pt>
2022-06-09 06:51:41 +0100

    criação das plataformas

--- script.js
+++ script.js
@@ -47,7 +47,24 @@ class Player {
     }
 }
 
+class Platform {
+    constructor(){
+        this.position = {
+            x: 200,
+            y: 100
+        }
+        this.width = 200
+        this.height = 20
+    }
+
+    draw(){
+        ctx.fillStyle = 'blue'
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
 const player = new Player()
+const platform = new Platform()
 const keys = {
     right: {
         pressed: false
@@ -73,12 +90,21 @@ function animate(){
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     player.update()
+    platform.draw()
 
     if (keys.right.pressed){
         player.velocity.x = 5
     } else if (keys.left.pressed){
         player.velocity.x = -5
     } else player.velocity.x = 0
+
+    //player platform collision
+    if (player.position.y + player.height <= platform.position.y 
+        && player.position.y + player.height + player.velocity.y >= platform.position.y
+        && player.position.x + player.width >= platform.position.x
+        && player.position.x  <= platform.position.x + platform.width){
+        player.velocity.y = 0
+    }
 };
 
 animate();             
